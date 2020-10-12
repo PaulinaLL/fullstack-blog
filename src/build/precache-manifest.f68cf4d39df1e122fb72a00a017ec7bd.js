319a854552562fb9f19e1be69995fe898717a6ed
PaulinaLL <paulinalll90@wp.pl>
2020-10-12 12:15:39 +0200

    first commit

---
+++ src/build/precache-manifest.f68cf4d39df1e122fb72a00a017ec7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31760aff8b73c5cce767732aef420eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c02849a90082565ab133",
+    "url": "/static/css/main.17c195d5.chunk.css"
+  },
+  {
+    "revision": "41b58b5cf45b1fdd4dcb",
+    "url": "/static/js/2.e3d77868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3d77868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02849a90082565ab133",
+    "url": "/static/js/main.514507c8.chunk.js"
+  },
+  {
+    "revision": "288b81f3f89e16098090",
+    "url": "/static/js/runtime-main.f266abb9.js"
+  }
+]);
